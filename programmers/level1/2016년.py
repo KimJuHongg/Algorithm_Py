f19ab85c91dba7a09b1609db8a6acdc2b7119d52
KimJuHongg <98217140+KimJuHongg@users.noreply.github.com>
2022-03-18 11:45:28 +0900

    Create 2016년.py

---
+++ programmers/level1/2016년.py
@@ -0,0 +1,8 @@
+import datetime
+def solution(a, b):
+    answer = ''
+    day = 'MON TUE WED THU FRI SAT SUN'.split()
+   
+    answer = day[datetime.datetime(2016,a,b).weekday()]
+    
+    return answer
